b3727d1c5b45919531689d11cddc87f123ed0eb2
Stiizzy Cat <90114741+StiizzyCat@users.noreply.github.com>
2021-09-18 19:37:11 -0700

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -5,7 +5,7 @@
 # ik this looka trash, im just starting to learn bash.
 echo " by pressing enter you agree to the terms [in the terms.sh]"
 read terms LOL
-printf "\e[93m\e[93m Id Hax - A Discord First half of token finder :) \e[0m\n" 
+printf "\e[93m\e[93m IdSploit - A Discord First half of token finder :) \e[0m\n" 
 printf        "\e[34m\e[34m        Coded By Stiizzy & Idea By Vapor \e[0m\n" 
 printf "\n"
 
@@ -14,4 +14,3 @@ echo "Please enter the userid you want encoded"
 read text
 etext=`echo -n $text | base64`
 echo "first half of the token is: $etext"
-
